0dbcf999e3519e3b06c96a66e340e9a8d7d884c2
Cezar T. <55374787+czarist@users.noreply.github.com>
2025-02-07 16:43:42 -0300

    weback

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+    entry: {
+        background: "./src/background.ts",
+        content: "./src/content.ts",
+        popup: "./src/popup.ts"
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].js"
+    },
+    resolve: {
+        extensions: [".ts", ".js"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [
+                { from: "src/manifest.json", to: "manifest.json" },
+                { from: "src/popup.html", to: "popup.html" },
+                { from: "src/icon.png", to: "icon.png" }
+            ]
+        })
+    ],
+    devtool: "inline-source-map"
+};
